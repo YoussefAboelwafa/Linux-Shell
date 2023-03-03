5ce2e66357562e42bdd49d0342697d1dfb8efb44
YoussefAboelwafa <youssef.hossam49@gmail.com>
2023-03-03 16:47:19 +0200

    create git ignore

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("hello youssef");
+    pid_t parent_id = getpid();
+
+    printf("parent, PID=%d  shell process id =%d \n", parent_id, getppid());
+    fork();
+    fork();
+    pid_t child_id = getpid();
+    pid_t current_parent = getppid();
+    if (current_parent == parent_id)
+    {
+        printf("\nchild, PID=%d  parent, PID=%d\n", child_id, parent_id);
+    }
+    fork();
     return 0;
 }
