294a75ef664e19f71716cedb0da60d5f119b6bc3
YoussefAboelwafa <youssef.hossam49@gmail.com>
2023-03-07 16:36:39 +0200

    background done

--- main.c
+++ main.c
@@ -24,8 +24,9 @@ char *value;
 int i;
 int k;
 FILE *file;
-int flag=0;
+int flag = 0;
 pid_t id;
+
 // functions prototypes
 void shell();
 void execute_command();
@@ -34,13 +35,13 @@ void reap_child_zombie(pid_t);
 void write_to_log_file(pid_t);
 void on_child_exit(pid_t);
 void on_signal_exit();
-void remove_quotes(char*);
-void replace_string(char*,char*,char*);
-char* extract_after_equal_sign(char*);
+void remove_quotes(char *);
+void replace_string(char *, char *, char *);
+char *extract_after_equal_sign(char *);
 
 int main()
-{   
-    signal (SIGCHLD, on_signal_exit);
+{
+    signal(SIGCHLD, on_signal_exit);
     file = fopen("logs.txt", "w");
     shell();
     return 0;
@@ -48,9 +49,9 @@ int main()
 
 void shell()
 {
-    
+
     while (true)
-    {   
+    {   flag=0;
         // Prompt the user for input
         char buf[1024];
         getcwd(buf, sizeof(buf));
@@ -77,12 +78,19 @@ void shell()
         i = 0;
 
         while (token != NULL)
-        {  
-            args[i++] = token;
+        {   
+                args[i++] = token;
+            
             token = strtok(NULL, " ");
         }
 
         args[i] = NULL;
+        
+        if(args[1]!=NULL){
+            if(strcmp(args[1],"&")==0){
+                flag=1;
+            }
+        }
 
         // Exit if the user enters "exit" as the command
         if (strcmp(args[0], "exit") == 0)
@@ -123,24 +131,24 @@ void shell_builtin_commands(char string[])
 
     else if (strcmp(string, "echo") == 0)
     {
-           
-        replace_string(args[1],"$x", value);
+
+        replace_string(args[1], "$x", value);
         // Otherwise, print the argument as is
         remove_quotes(args[1]);
-        printf("%s\n", args[1]); 
+        printf("%s\n", args[1]);
     }
     else if (strcmp(string, "export") == 0)
-    {   
+    {
         value = extract_after_equal_sign(args[1]);
         remove_quotes(value);
-        printf("%s\n",value);       
+        printf("%s\n", value);
     }
 }
 
 void execute_command()
-{   
+{
     pid_t pid = fork();
-    id=pid;
+    id = pid;
     // Error
     if (pid < 0)
     {
@@ -149,10 +157,8 @@ void execute_command()
     }
     // In the child
     else if (pid == 0)
-     {
-        //  if(strcmp(args[1],"&")==0){
-        //     flag=1;
-        //  }
+    { 
+        
         if (execvp(args[0], args) == -1)
         {
             perror("Failed to execute command");
@@ -161,41 +167,47 @@ void execute_command()
     }
     // In the parent
     else
-    {   
-        
-        on_child_exit(pid);
-        
-        
+    {
+        if(flag==0){
+            on_child_exit(pid);
+        }
     }
 }
 
-void reap_child_zombie(pid_t pid){
-    int status;   
-    waitpid(0, &status, 0);  
+void reap_child_zombie(pid_t pid)
+{
+    int status;
+    waitpid(0, &status, 0);
 }
-void write_to_log_file(pid_t pid){
+void write_to_log_file(pid_t pid)
+{
     fprintf(file, "Child process (%d) terminated\n", pid);
 }
-void on_child_exit(pid_t pid){
-    //printf("child %d\n",pid);
-    reap_child_zombie(pid); 
+void on_child_exit(pid_t pid)
+{
+    // printf("child %d\n",pid);
+    reap_child_zombie(pid);
     write_to_log_file(pid);
-    
 }
-void on_signal_exit(){
-    //printf("child %d\n",id);
+void on_signal_exit()
+{
+    // printf("child %d\n",id);
     int var;
     int status;
-    var=waitpid(0,&status,0);
-    if(var>=0){
+    var = waitpid(0, &status, 0);
+    if (var >= 0)
+    {
         write_to_log_file(id);
     }
 }
 
-void remove_quotes(char* str) {
-    char* dest = str;
-    while (*str) {
-        if (*str != '"') {
+void remove_quotes(char *str)
+{
+    char *dest = str;
+    while (*str)
+    {
+        if (*str != '"')
+        {
             *dest++ = *str;
         }
         str++;
@@ -203,29 +215,37 @@ void remove_quotes(char* str) {
     *dest = '\0';
 }
 
-void replace_string(char* original_string, char* find_string, char* replace_string) {
-    char* occurrence = strstr(original_string, find_string); 
-    size_t find_len = strlen(find_string); 
-    
-    while (occurrence != NULL) { 
-        size_t occurrence_index = occurrence - original_string; 
-        size_t replace_len = strlen(replace_string); 
-        
-        memmove(original_string + occurrence_index + replace_len, 
+void replace_string(char *original_string, char *find_string, char *replace_string)
+{
+    char *occurrence = strstr(original_string, find_string);
+    size_t find_len = strlen(find_string);
+
+    while (occurrence != NULL)
+    {
+        size_t occurrence_index = occurrence - original_string;
+        size_t replace_len = strlen(replace_string);
+
+        memmove(original_string + occurrence_index + replace_len,
                 original_string + occurrence_index + find_len,
                 strlen(original_string + occurrence_index + find_len) + 1);
-                
-        memcpy(original_string + occurrence_index, replace_string, replace_len); 
-        
+
+        memcpy(original_string + occurrence_index, replace_string, replace_len);
+
         occurrence = strstr(original_string + occurrence_index + replace_len, find_string);
     }
 }
-char* extract_after_equal_sign(char* input_string) {
-    char* occurrence = strchr(input_string, '='); // find the first occurrence of '='
-    
-    if (occurrence != NULL) { // check if '=' was found
+char *extract_after_equal_sign(char *input_string)
+{
+
+    char *occurrence = strchr(input_string, '='); // find the first occurrence of '='
+
+    if (occurrence != NULL)
+    {                          // check if '=' was found
         return occurrence + 1; // return the substring that follows the '='
-    } else {
+    }
+    else
+    {
         return input_string; // return the whole input string if '=' was not found
     }
 }
+
